c3438296c1d380ab2e81682cc0f3098a6fe36141
boubou417 <51277941+boubou417@users.noreply.github.com>
2026-08-26 22:09:22 +0800

    Route Google Wallet sync facade to V7.2

--- android/app/src/main/java/tw/apostar/notificationpaytest/GoogleWalletSyncControllerV3.kt
+++ android/app/src/main/java/tw/apostar/notificationpaytest/GoogleWalletSyncControllerV3.kt
@@ -3,14 +3,14 @@ package tw.apostar.notificationpaytest
 /** Compatibility facade used by the existing Capacitor bridge. */
 object GoogleWalletSyncControllerV3 {
     @JvmStatic
-    fun isRunning(): Boolean = GoogleWalletSyncControllerV71.isRunning()
+    fun isRunning(): Boolean = GoogleWalletSyncControllerV72.isRunning()
 
     @JvmStatic
-    fun start(s: PayAccessibilityService) = GoogleWalletSyncControllerV71.start(s)
+    fun start(s: PayAccessibilityService) = GoogleWalletSyncControllerV72.start(s)
 
     @JvmStatic
-    fun stop(returnToApp: Boolean = true) = GoogleWalletSyncControllerV71.stop(returnToApp)
+    fun stop(returnToApp: Boolean = true) = GoogleWalletSyncControllerV72.stop(returnToApp)
 
     @JvmStatic
-    fun poke() = GoogleWalletSyncControllerV71.poke()
+    fun poke() = GoogleWalletSyncControllerV72.poke()
 }
