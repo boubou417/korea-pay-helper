edfa14f4b9fd9dde31216680fdf6a73a6fdc4a4c
boubou417 <51277941+boubou417@users.noreply.github.com>
2026-08-26 21:34:58 +0800

    Route Google Wallet sync through V7.1 backfill controller

--- android/app/src/main/java/tw/apostar/notificationpaytest/GoogleWalletSyncControllerV3.kt
+++ android/app/src/main/java/tw/apostar/notificationpaytest/GoogleWalletSyncControllerV3.kt
@@ -3,14 +3,14 @@ package tw.apostar.notificationpaytest
 /** Compatibility facade used by the existing Capacitor bridge. */
 object GoogleWalletSyncControllerV3 {
     @JvmStatic
-    fun isRunning(): Boolean = GoogleWalletSyncControllerV7.isRunning()
+    fun isRunning(): Boolean = GoogleWalletSyncControllerV71.isRunning()
 
     @JvmStatic
-    fun start(s: PayAccessibilityService) = GoogleWalletSyncControllerV7.start(s)
+    fun start(s: PayAccessibilityService) = GoogleWalletSyncControllerV71.start(s)
 
     @JvmStatic
-    fun stop(returnToApp: Boolean = true) = GoogleWalletSyncControllerV7.stop(returnToApp)
+    fun stop(returnToApp: Boolean = true) = GoogleWalletSyncControllerV71.stop(returnToApp)
 
     @JvmStatic
-    fun poke() = GoogleWalletSyncControllerV7.poke()
+    fun poke() = GoogleWalletSyncControllerV71.poke()
 }
