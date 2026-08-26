64b47cab9b05d103b2572d12bccf731506a96fc9
boubou417 <51277941+boubou417@users.noreply.github.com>
2026-08-26 23:54:29 +0800

    Fix MainActivity onResume visibility

--- android/app/src/main/java/com/bou/payhelper/MainActivity.java
+++ android/app/src/main/java/com/bou/payhelper/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         NightlySyncReceiver.scheduleNext(this);
     }
